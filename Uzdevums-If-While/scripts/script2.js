7638f6135f7a3437df0787ccb400214bc5c15780
Nikn1977 <nikn13@inbox.lv>
2024-11-11 15:07:55 +0200

    Add files via upload

---
+++ Uzdevums-If-While/scripts/script2.js
@@ -0,0 +1,6 @@
+var a = 1, b = 10;
+
+if (a < b) { 
+ let test_dilstosa_sec = [ b, a];
+document.getElementById("dilstosa_sec").innerHTML = `Pirms kartošanas: a = ${a}, b = ${b} <br>Dilstoša seciba: ${test_dilstosa_sec}<br>`;
+} 
